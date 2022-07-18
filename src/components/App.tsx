be08baf515b616a48379b43ef9fda074876443d1
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-07-18 21:59:55 +0300

    Created app creact and button component

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from "./Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>My React App</h1>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
