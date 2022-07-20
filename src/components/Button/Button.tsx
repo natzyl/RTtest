12ecb5791c7903bc029d4183170060d89d14fe7d
John Doe <natalia.zyl19@gmaol.com>
2022-07-20 19:51:54 +0300

    remove button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const Button = () => {
-<<<<<<< HEAD
-    return <button className="my-button">Click me</button>;
-=======
-    return <button className="my-button">Click please me!</button>;
->>>>>>> 9155c92329b69e3d8331d29bbe092c9593bb7adb
-};
+    return <button className="my-button">Click</button>
+}
 
 export default Button;
