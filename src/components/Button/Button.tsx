9c825158c4d0dad0cd017df4886a81b1192993c7
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-07-20 19:37:58 +0300

    Add words

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const Button = () => {
-    return <button className="my-button">Click</button>;
+    return <button className="my-button">Click please me!</button>;
 };
 
 export default Button;
