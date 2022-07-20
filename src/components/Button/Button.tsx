ddd7474d39303f5736649ea820ba8cb13dc89de8
John Doe <natalia.zyl19@gmaol.com>
2022-07-20 20:15:51 +0300

    add

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Button.css';
 
 const Button = () => {
-    return <button className="my-button">Click</button>
+     const [buttonText, setButtonText] = useState('Click please me!');
+    return (
+        <button className="my-button" 
+        onClick={() => console.log('click')}
+        >
+            {buttonText}
+        </button>
+    )
 }
 
 export default Button;
