939b2c27f1a711ca3af44af34dde509809a0e5f2
John Doe <natalia.zyl19@gmaol.com>
2022-07-20 21:50:31 +0300

    new

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Button from "./Button/Button";
 import ButtonProps from './ButtoProps/ButtonProps';
+import Counter from './Counter/Counter';
+
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <h1>My React App</h1>
       <Button/>
       <ButtonProps></ButtonProps>
+      <Counter></Counter>
     </div>
   );
 }
