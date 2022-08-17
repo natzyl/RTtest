a6d96ba0292336e4d99f3a6c18b5ef47e7ec555f
John Doe <natalia.zyl19@gmaol.com>
2022-08-17 20:26:21 +0300

    feature search user and add user form

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,87 @@
+import React, {FC, useState, useMemo} from 'react'
+import { USERS } from './usersData'
+import { IUser } from './IUser';
+ 
+
+const Users:FC = () => {
+    const [users, setUsers] = useState<IUser[]>(USERS);
+    const [search, setSearch] = useState('');
+    const[showUserForm, setShowUserForm] = useState(false);
+    const deleteUser = (id:number) => {
+        const isDelete = window.confirm("Do you realy delete this user?");
+        if(isDelete) {
+            setUsers(users.filter(user => user.id !== id));
+        }
+    }
+    const searchedUsers = useMemo( () => {
+      if (search) {
+        return users.filter(user => user.name.toLowerCase().includes(search.toLowerCase()));
+      }
+      return users;
+    }, [search, users]);
+    console.log(searchedUsers);
+    return (
+        <>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">@</span>
+                <input type="text" 
+                        className="form-control" 
+                        placeholder="Username" 
+                        aria-label="Username" 
+                        aria-describedby="basic-addon1"
+                        onChange={(event) => setSearch(event.target.value)}
+  />
+          </div>
+
+  <button className="btn btn-success mt-3 mb-3" 
+        onClick={() => setShowUserForm(!showUserForm)}
+  >
+        Add new User
+  </button>
+
+  {showUserForm && 
+  <form>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Add</button>
+  </form>
+  }
+
+  
+
+<div className="row row-cols-1 row-cols-md-3 g-4">Add
+    {users.length 
+    ? 
+    searchedUsers.map(user => 
+  <div className="col" key={user.id}>
+    <div className="card h-100">
+      <div className="card-body">
+        <h5 className="card-title">{`№${user.id} - ${user.name}`}</h5>
+        <p className="card-text">Email: {user.email}</p>
+        <p className="card-text">City: {user.address.city}</p>
+        <p className="card-text">Company: {user.company.name}</p>
+      </div>
+      <div className="card-footer">
+       <button className="btn btn-danger" 
+            onClick ={() => deleteUser(user.id)}
+       >
+           Delete
+        </button>
+      </div>
+    </div>
+  </div>
+)
+:
+<h2>Users not exist</h2>
+}
+  </div>
+  </>
+)
+}
+
+export default Users
